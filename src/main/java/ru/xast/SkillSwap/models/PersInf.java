2ae289983bf672d747ccfde6012064714a76b83f
Xast <artemhasrovyan@gmail.com>
2025-02-08 17:34:04 +0300

    fix lombok

--- src/main/java/ru/xast/SkillSwap/models/PersInf.java
+++ src/main/java/ru/xast/SkillSwap/models/PersInf.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-//@Getter
-//@Setter
+@Getter
+@Setter
 @Table(name = "persinf")
 public class PersInf {
 
@@ -57,59 +57,4 @@ public class PersInf {
         this.email = email;
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getBirthYear() {
-        return birthYear;
-    }
-
-    public void setBirthYear(int birthYear) {
-        this.birthYear = birthYear;
-    }
-
-    public List<ProfInf> getProvidedSkills() {
-        return providedSkills;
-    }
-
-    public void setProvidedSkills(List<ProfInf> providedSkills) {
-        this.providedSkills = providedSkills;
-    }
 }
